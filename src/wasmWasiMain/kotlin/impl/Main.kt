f8dbd97574918ba09774b769c41cf30e5c977b42
Jim M. R. Teichgräber <jim.teichgraeber@jetbrains.com>
2026-03-03 13:34:09 +0100

    Nicer implementation

--- src/wasmWasiMain/kotlin/impl/Main.kt
+++ src/wasmWasiMain/kotlin/impl/Main.kt
@@ -1,6 +1,8 @@
 package impl
 
-import bindings.*
+import bindings.ComponentException
+import bindings.IncomingHandlerExports
+import bindings.Types
 
 // impl based on https://github.com/bytecodealliance/sample-wasi-http-rust/blob/53279f531cfa9c8e88c172f3c98a21e002246bbb/src/lib.rs
 
@@ -8,24 +10,41 @@ object IncomingHandlerExportsImpl : IncomingHandlerExports {
     override fun handle(request: Types.IncomingRequest, responseOut: Types.ResponseOutparam) {
         when(request.pathWithQuery()) {
             // right now, just respond to everything with home
-            else -> httpHome(request, responseOut)
+            "/" -> httpResponse(request, responseOut, 200){
+                "Hello, wasi:http from Kotlin!"
+            }
+            else -> httpResponse(request, responseOut, 404, "<h1>Sad 404 noises</h1>")
         }
     }
 
-    fun httpHome(request: Types.IncomingRequest, outparam: Types.ResponseOutparam) {
+    fun httpResponse(request: Types.IncomingRequest, outparam: Types.ResponseOutparam, statusCode:Int, responseBuiler: () -> String){
         val headers = Types.Fields()
         val response = Types.OutgoingResponse(headers)
+        response.setStatusCode(statusCode.toUShort())
         val body = response.body().getOrThrow()
 
-        Types.ResponseOutparam.set(outparam, Result.success(response))
+        try{
+            Types.ResponseOutparam.set(outparam, Result.success(response))
 
-        val out = body.write().getOrThrow()
-        out.use {
-            out.blockingWriteAndFlush("Hello, wasi:http/proxy world from kotlin!\n".toList().map { it.code.toUByte() })
+            val out = body.write().getOrThrow()
+            out.use {
+                out.blockingWriteAndFlush(responseBuiler().toList().map { it.code.toUByte() })
+            }
+
+            Types.OutgoingBody.finish(body, null)
+        }catch (e:Throwable){
+            // TODO exception catching doesn't seem to work yet, even if we just `throw Exception("lala") above
+            val errorCode = if(e is ComponentException && e.value is Types.ErrorCode) {
+                e.value
+            }else{
+                Types.ErrorCode.InternalError("Let's leak the stack trace yaaaay: ${e.stackTraceToString()}")
+            }
+            Types.ResponseOutparam.set(outparam, Result.failure(ComponentException(errorCode)))
         }
-
-        Types.OutgoingBody.finish(body, null)
     }
+
+    fun httpResponse(request: Types.IncomingRequest, outparam: Types.ResponseOutparam, statusCode:Int, responseBody: String) =
+        httpResponse(request, outparam, statusCode) { responseBody }
 
 }
 
