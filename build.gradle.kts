a738fa09421f5d29318c0b5ab6edac029b335cf5
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2026-03-08 11:02:02 +0100

    Update Kotlin toolchain version to 2.4.0-dev-5756

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
-    kotlin("multiplatform") version "2.4.0-dev-5743"
+    kotlin("multiplatform") version "2.4.0-dev-5756"
 }
 
 repositories {
