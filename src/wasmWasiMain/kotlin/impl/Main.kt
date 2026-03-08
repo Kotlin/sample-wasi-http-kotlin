8f8b5808f5b18246befaee98c19b2b48e312f133
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2026-03-08 21:58:38 +0100

    Make the response DSL more composable
    Cleanup code a bit.

--- src/wasmWasiMain/kotlin/impl/Main.kt
+++ src/wasmWasiMain/kotlin/impl/Main.kt
@@ -1,31 +1,24 @@
 package impl
 
 import bindings.*
+import bindings.Types.*
 
 // implementation based on https://github.com/bytecodealliance/sample-wasi-http-rust/blob/53279f531cfa9c8e88c172f3c98a21e002246bbb/src/lib.rs
 
-fun debugPrint(msg: String) {
-    Stdout.getStdout().blockingWriteAndFlush(msg.map { it.code.toUByte() })
-}
-
-fun debugPrintLn(msg: String) {
-    debugPrint("$msg\n")
-}
-
 object IncomingHandlerImpl : IncomingHandler {
-    override fun handle(request: Types.IncomingRequest, responseOut: Types.ResponseOutparam) {
+    override fun handle(request: IncomingRequest, responseOut: ResponseOutparam) {
         when (val path = request.pathWithQuery()) {
-            "/throw-exception" -> httpResponse(request, responseOut){
-                throw ComponentException(Types.ErrorCode.InternalError("This is a test exception"))
+            "/throw-exception" -> responseOut.response {
+                throw ComponentException(ErrorCode.InternalError("This is a test exception"))
             }
-            "/" -> httpResponse(request, responseOut) {
+
+            "/" -> responseOut.response(200) {
                 val W = WallClock.now()
 
                 val startNs = W.seconds.toLong() * 1_000_000_000L + W.nanoseconds.toLong()
 
-                Response(
-                    statusCode = 200, body =
-                        """
+                body(
+                    """
                     <p>Hello, wasi:http from Kotlin!</p>
                     <p>Assuming our clocks are perfectly in sync, the server took roughly 
                     <script>
@@ -46,67 +39,66 @@ object IncomingHandlerImpl : IncomingHandler {
                     """.trimIndent()
                 )
             }
-            "/echo" -> httpResponse(request, responseOut) {
-                Response(statusCode = 200, body = request.consume().getOrThrow().use { incomingBody ->
-                    incomingBody.stream().getOrThrow().use { stream ->
-                        // basically: stop reading if the list if read either returns failure, or an empty list
-                        val inputBytes = generateSequence { stream.read(1u).getOrNull()?.singleOrNull() }
-                        inputBytes.map { it.toInt().toChar() }.joinToString("")
-                    }
-                })
+
+            "/echo" -> responseOut.response(200) {
+                body(
+                    request.consume().getOrThrow().use { incomingBody ->
+                        incomingBody.stream().getOrThrow().use { stream ->
+                            // basically: stop reading if the list if read either returns failure, or an empty list
+                            val inputBytes = generateSequence { stream.read(1u).getOrNull()?.singleOrNull() }
+                            inputBytes.map { it.toInt().toChar() }.joinToString("")
+                        }
+                    })
             }
-            "/echo-headers" -> httpResponse(request, responseOut) {
-                Response(statusCode = 200, headers = request.headers(), body = "")
-            }
+
+            "/echo-headers" -> responseOut.response(200, request.headers(), "")
+
             else -> {
                 debugPrintLn("Unknown path: $path")
-                httpResponse(request, responseOut, 404, "<h1>Sad 404 noises</h1>")
+                responseOut.response(404, content = "<h1>Sad 404 noises</h1>")
             }
         }
     }
-
-    data class Response(val statusCode: Int, val headers: Types.Fields = Types.Fields(), val body: String)
-
-    fun httpResponse(
-        request: Types.IncomingRequest,
-        outparam: Types.ResponseOutparam,
-        responseBuilder: () -> Response
-    ) {
-        try {
-            val userResponse = responseBuilder()
-            val response = Types.OutgoingResponse(userResponse.headers)
-            response.setStatusCode(userResponse.statusCode.toUShort())
-            val body = response.body().getOrThrow()
-
-            Types.ResponseOutparam.set(outparam, Result.success(response))
-
-            val out = body.write().getOrThrow()
-            out.use {
-                out.blockingWriteAndFlush(userResponse.body.toList().map { it.code.toUByte() })
-            }
-
-            Types.OutgoingBody.finish(body, null)
-        } catch (e: Throwable) {
-            // TODO exception catching doesn't seem to work yet, even if we just `throw Exception("lala") above
-            val errorCode = if (e is ComponentException && e.value is Types.ErrorCode) {
-                e.value
-            } else {
-                Types.ErrorCode.InternalError("Let's leak the stack trace yaaaay: ${e.stackTraceToString()}")
-            }
-            Types.ResponseOutparam.set(outparam, Result.failure(ComponentException(errorCode)))
-        }
-    }
-
-    fun httpResponse(
-        request: Types.IncomingRequest,
-        outparam: Types.ResponseOutparam,
-        statusCode: Int,
-        responseBody: String
-    ) =
-        httpResponse(request, outparam) { Response(statusCode, body = responseBody) }
-
 }
 
-fun main() {
+fun ResponseOutparam.response(statusCode: Int = 200, headers: Fields = Fields(), build: OutgoingResponse.() -> Unit) {
+    val response = OutgoingResponse(headers)
+    try {
+        response.setStatusCode(statusCode.toUShort())
+        response.build()
+        ResponseOutparam.set(this, Result.success(response))
+    } catch (e: Throwable) {
+        response.close()
+        val errorCode = if (e is ComponentException && e.value is ErrorCode) {
+            e.value
+        } else {
+            ErrorCode.InternalError("Unexpected error: $e")
+        }
+        ResponseOutparam.set(this, Result.failure(ComponentException(errorCode)))
+    }
+}
 
+fun OutgoingResponse.body(build: Streams.OutputStream.() -> Unit) {
+    val body = body().getOrThrow()
+    val out: Streams.OutputStream = body.write().getOrThrow()
+    out.use(build)
+    OutgoingBody.finish(body, null)
+}
+
+fun ResponseOutparam.response(statusCode: Int = 200, headers: Fields = Fields(), content: String) {
+    response(statusCode, headers) { body(content) }
+}
+
+fun OutgoingResponse.body(content: String) {
+    body {
+        blockingWriteAndFlush(content.map { it.code.toUByte() })
+    }
+}
+
+fun debugPrint(msg: String) {
+    Stdout.getStdout().blockingWriteAndFlush(msg.map { it.code.toUByte() })
+}
+
+fun debugPrintLn(msg: String) {
+    debugPrint("$msg\n")
 }
