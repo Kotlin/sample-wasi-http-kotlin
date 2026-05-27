c4d1bd100e14cc5d959cef23e40a4b677c1b6d01
Jim M. R. Teichgräber <jim.teichgraeber@jetbrains.com>
2026-05-27 19:16:07 +0200

    Adapt to wit-bindgen changes by adding import bindings.runtime.*

--- src/wasmWasiMain/kotlin/impl/Main.kt
+++ src/wasmWasiMain/kotlin/impl/Main.kt
@@ -2,6 +2,7 @@ package impl
 
 import bindings.*
 import bindings.Types.*
+import bindings.runtime.*
 
 // implementation based on https://github.com/bytecodealliance/sample-wasi-http-rust/blob/53279f531cfa9c8e88c172f3c98a21e002246bbb/src/lib.rs
 
